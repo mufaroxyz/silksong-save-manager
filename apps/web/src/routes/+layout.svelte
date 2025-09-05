372dbdca65eebc439d8060a8aa19885bbf938794
Mufaro <81554673+mufaroxyz@users.noreply.github.com>
2025-09-05 08:25:39 +0000

    fix: removed file not updated in layout.svelte

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import '../app.css';
-    import Header from '../components/Header.svelte';
-
+	import "../app.css";
 	let { children } = $props();
 </script>
 
 <div class="grid h-svh grid-rows-[auto_1fr]">
-	<Header />
 	<main class="overflow-y-auto">
 		{@render children()}
 	</main>
